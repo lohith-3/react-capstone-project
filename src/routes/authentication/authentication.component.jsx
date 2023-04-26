3a71a98620e63c15b6769f423a9cf5abc34e6ee3
lohith <lohithreddy.e@gmail.com>
2023-04-26 09:56:02 +0530

    added a authentication componeny, sign in and sign up form and added error code and styled the authentication componeny

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,27 +1,30 @@
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils.js";
+// import {
+//   signInWithGooglePopup,
+//   createUserDocumentFromAuth,
+// } from "../../utils/firebase/firebase.utils.js";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component.jsx";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component.jsx";
 
-const SignIn = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-    console.log(userDocRef);
-  };
+import "./authentication.styles.scss";
 
+const Authentication = () => {
   return (
-    <div>
-      <h1>Sign in page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    <div className="authentication-container">
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
+
+// const logGoogleUser = async () => {
+//   const { user } = await signInWithGooglePopup();
+//   const userDocRef = await createUserDocumentFromAuth(user);
+//   console.log(userDocRef);
+// };
+/* <button onClick={logGoogleUser}>Sign in with Google Popup</button> */
 
 // import { useEffect } from "react";
 // import { getRedirectResult } from "firebase/auth";
