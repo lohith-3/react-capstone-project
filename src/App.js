3821d0319cbc555142fdcd83d627c9cd4b8dd941
lohith <lohithreddy.e@gmail.com>
2023-05-06 19:00:11 +0530

    adding cart context, shopping context and cart dropdown component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
-const Shop = () => {
-  return (
-    <>
-      <h1>I am the shop page</h1>
-    </>
-  );
-};
+// import { ProductsProvider } from "./context/products.context";
 
 const App = () => {
   return (
@@ -19,7 +14,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />}></Route>
+
           <Route path="shop" element={<Shop />}></Route>
+
           <Route path="auth" element={<Authentication />}></Route>
         </Route>
       </Routes>
